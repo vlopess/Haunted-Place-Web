c09fba76bd096cabf1c322d47511ed824f50ad11
victor <victorlopes9291@gmail.com>
2024-06-05 22:11:13 -0300

    refact: atualizando o ChipsTagComponent e estilizando o mesmo

--- src/app/components/register/chips-tag/chips-tag.component.ts
+++ src/app/components/register/chips-tag/chips-tag.component.ts
@@ -1,85 +1,58 @@
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {Component, ElementRef, ViewChild, inject} from '@angular/core';
-import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatAutocompleteSelectedEvent, MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatChipInputEvent, MatChipsModule} from '@angular/material/chips';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+import {Component} from '@angular/core';
+import {MatChipEditedEvent, MatChipInputEvent, MatChipsModule} from '@angular/material/chips';
 import {MatIconModule} from '@angular/material/icon';
-import {AsyncPipe} from '@angular/common';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {LiveAnnouncer} from '@angular/cdk/a11y';
+
 
 /**
- * @title Chips Autocomplete
+ * @title Chips with input
  */
 @Component({
   selector: 'app-chips-tag',
-  templateUrl: './chips-tag.component.html',
-  styleUrl: './chips-tag.component.css',
+  templateUrl: 'chips-tag.component.html',
+  styleUrl: 'chips-tag.component.css',
   standalone: true,
-  imports: [
-    FormsModule,
-    MatFormFieldModule,
-    MatChipsModule,
-    MatIconModule,
-    MatAutocompleteModule,
-    ReactiveFormsModule,
-    AsyncPipe,
-  ],
+  imports: [MatFormFieldModule, MatChipsModule, MatIconModule],
 })
 export class ChipsTagComponent {
-  separatorKeysCodes: number[] = [ENTER, COMMA];
-  tagCtrl = new FormControl('');
-  filteredTags: Observable<string[]>;
-  tags: string[] = [];
-    allTags: string[] = ['Music', 'Movies', 'Style', 'Literature'];
-
-  // @ts-ignore
-  @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
-
-  announcer = inject(LiveAnnouncer);
-
-  constructor() {
-    this.filteredTags = this.tagCtrl.valueChanges.pipe(
-      startWith(null),
-      map((tag: string | null) => (tag ? this._filter(tag) : this.allTags.slice())),
-    );
-  }
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  tags: string[] = ['Music','Movies','Style','Literature'];
 
   add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
 
-    // Add our fruit
+    // Add our tag
     if (value) {
       this.tags.push(value);
     }
 
     // Clear the input value
     event.chipInput!.clear();
-
-    this.tagCtrl.setValue(null);
   }
 
-  remove(fruit: string): void {
-    const index = this.tags.indexOf(fruit);
+  remove(tag: string): void {
+    const index = this.tags.indexOf(tag);
 
     if (index >= 0) {
       this.tags.splice(index, 1);
-
-      this.announcer.announce(`Removed ${fruit}`).then();
     }
   }
 
-  selected(event: MatAutocompleteSelectedEvent): void {
-    this.tags.push(event.option.viewValue);
-    this.fruitInput.nativeElement.value = '';
-    this.tagCtrl.setValue(null);
-  }
+  edit(tag: string, event: MatChipEditedEvent) {
+    const value = event.value.trim();
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
+    // Remove tag if it no longer has a name
+    if (!value) {
+      this.remove(tag);
+      return;
+    }
 
-    return this.allTags.filter(fruit => fruit.toLowerCase().includes(filterValue));
+    // Edit existing tag
+    const index = this.tags.indexOf(tag);
+    if (index >= 0) {
+      this.tags[index] = value;
+    }
   }
 }
